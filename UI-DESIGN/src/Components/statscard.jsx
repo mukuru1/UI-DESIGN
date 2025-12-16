a84c1782bde099acbeac64dae3b0dc78e0526084
mukuru1 <kabengaedwin@gmail.com>
2025-12-16 21:12:41 +0200

    commit

---
+++ UI-DESIGN/src/Components/statscard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function StatsCard({ title, value, color }) {
+  const colorStyles = {
+    blue: 'bg-primary-50 border-primary-200',
+    green: 'bg-green-50 border-green-200',
+    purple: 'bg-purple-50 border-purple-200',
+    orange: 'bg-orange-50 border-orange-200',
+  }
+
+  return (
+      
+  )
