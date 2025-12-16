d4ab7106eea8be353a5398347a79374b54dc0669
mukuru1 <kabengaedwin@gmail.com>
2025-12-16 21:43:18 +0200

    commit

---
+++ UI-DESIGN/src/Components/recentactivity.jsx
@@ -0,0 +1,14 @@
+const activities = [
+  { 
+    title: 'Product added to inventory',
+    description: 'MacBook Pro 16" M3 (PROD2024001)',
+    date: 'Dec 4, 2024'
+  },
+  
+
+  { 
+    title: 'Product assigned to Sarah Johnson',
+    description: 'Dell ThinkPad X1 Carbon (PROD2024001)',
+    date: 'Dec 3, 2024'
+  },
+
