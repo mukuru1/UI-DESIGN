f4ffe8d64cc6fb72044c8994d39412d84c611dd9
mukuru1 <kabengaedwin@gmail.com>
2025-12-16 21:49:56 +0200

    commit

---
+++ UI-DESIGN/src/Components/quickactions.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const actions = [
+  { title: 'View Users', description: 'View all registered users' },
+  { title: 'View Products', description: 'View all registered products' },
+  { title: 'View Assignments', description: 'View all product assignments' },
+]
